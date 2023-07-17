a869c8687376520d05f4052479ece87a135d7e54
bmuday <bmuday@live.fr>
2023-07-17 16:04:28 +0200

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,18 +3,20 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export const metadata = {
-  title: "Flexibble",
+  title: "Next13 Flexibble",
   description: "Showcase and discover remarkable developer projects",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body>
         <Navbar />
-        <main>
-          {children}
-        </main>
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
